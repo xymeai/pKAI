4ebbd509d3e5d064436bbdafd477ba697c0283f9
Emlyn Clay <eclay@xyme.ai>
2025-10-01 16:40:21 +0100

    ENG-240: feat: Move pkai to __init__.py

--- src/pkai/__init__.py
+++ src/pkai/__init__.py
@@ -1,5 +1,28 @@
+from os.path import dirname, abspath
+
+import torch
+
+from pkai.protein import Protein
+
 name = "pkai"
 
-import os, sys
 
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+def load_model(model_name: str, device):
+    path = dirname(abspath(__file__))
+    fname = f"{model_name}_model.pt"
+    fpath = f"{path}/models/{fname}"
+
+    device = torch.device(device)
+    model = torch.jit.load(f"{fpath}").to(device)
+
+    return model
+
+
+def pkai(pdb, model_name="pkai", device="cpu", threads=None):
+    if threads:
+        torch.set_num_threads(threads)
+    model = load_model(model_name, device)
+    prot = Protein(pdb)
+    prot.apply_cutoff()
+    pks = prot.predict_pkas(model, device)
+    return pks
--- src/pkai/__main__.py
+++ src/pkai/__main__.py
@@ -1,30 +1,6 @@
 import argparse
-from os.path import dirname, abspath
 
-import torch
-
-from pkai.protein import Protein
-
-
-def load_model(model_name: str, device):
-    path = dirname(abspath(__file__))
-    fname = f"{model_name}_model.pt"
-    fpath = f"{path}/models/{fname}"
-
-    device = torch.device(device)
-    model = torch.jit.load(f"{fpath}").to(device)
-
-    return model
-
-
-def pkai(pdb, model_name="pkai", device="cpu", threads=None):
-    if threads:
-        torch.set_num_threads(threads)
-    model = load_model(model_name, device)
-    prot = Protein(pdb)
-    prot.apply_cutoff()
-    pks = prot.predict_pkas(model, device)
-    return pks
+from pkai import pkai
 
 
 def main():
